6282235aff81407e04ee35cf6ae73844ce351ccf
dimitar <zeitgeistdimi@gmail.com>
2018-11-04 15:59:06 +0200

    adding available seats column in Projection table. Functionality for available seats count.

---
+++ CinemAPI.Models/Projection.cs
@@ -0,0 +1,52 @@
+﻿using CinemAPI.Models.Contracts.Projection;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemAPI.Models
+{
+    public class Projection : IProjection, IProjectionCreation
+    {
+        private int availableSeatsCount;
+
+        public Projection()
+        {
+        }
+
+        public Projection(int movieId, int roomId, DateTime startdate, int availableSeatsCount)
+        {
+            this.MovieId = movieId;
+            this.RoomId = roomId;
+            this.StartDate = startdate;
+            this.AvailableSeatsCount = availableSeatsCount;
+        }
+
+        public long Id { get; set; }
+
+        public int RoomId { get; set; }
+
+        public virtual Room Room { get; set; }
+
+        public int MovieId { get; set; }
+
+        public virtual Movie Movie { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        [Range(0,int.MaxValue, ErrorMessage = "It sucks to eat popcorns, standing up ;) Cinema needs seats!")]
+        public int AvailableSeatsCount
+        {
+            get;
+            //{
+            //    return this.availableSeatsCount;
+            //}
+            set;
+            //{
+            //    if (value < 0)
+            //    {
+            //        throw new ArgumentException("Projection with no available seats is not accepted!");
+            //    };
+            //    this.availableSeatsCount = value;
+            //}
+        }
+    }
+}
---
+++ CinemAPI/Controllers/ProjectionController.cs
@@ -0,0 +1,70 @@
+﻿using CinemAPI.Data;
+using CinemAPI.Domain.Contracts;
+using CinemAPI.Domain.Contracts.Models;
+using CinemAPI.Models;
+using CinemAPI.Models.Contracts.Projection;
+using CinemAPI.Models.Input.Projection;
+using System;
+using System.Web.Http;
+
+namespace CinemAPI.Controllers
+{
+    public class ProjectionController : ApiController
+    {
+        private readonly INewProjection newProj;
+        private readonly IProjectionRepository projectionRepo;
+
+        public ProjectionController(INewProjection newProj, IProjectionRepository projectionRepo)
+        {
+            this.newProj = newProj;
+            this.projectionRepo = projectionRepo;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Index(ProjectionCreationModel model)
+        {
+            NewProjectionSummary summary = newProj.New(new Projection(model.MovieId, model.RoomId, model.StartDate, model.AvailableSeatsCount));
+
+            if (summary.IsCreated)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(summary.Message);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult SeatsCount(int id)
+        {
+            IProjection projection = projectionRepo.GetById(id);
+
+            if (projection.StartDate <= DateTime.UtcNow)
+            {
+                return BadRequest("Check the timeline Dummy :) It looks like you missed that one!");
+            }
+
+            if (projection.AvailableSeatsCount == 0)
+            {
+                return Ok($"Next time be faster. Available Seats:{projection.AvailableSeatsCount}");
+            }
+
+            return Ok($"It looks like it is your lucky day. Available Seats: {projection.AvailableSeatsCount}");
+        }
+        //[HttpPost]
+        //public IHttpActionResult Create(ProjectionCreationModel model)
+        //{
+        //    NewProjectionSummary summary = newProj.New(new Projection(model.MovieId, model.RoomId, model.StartDate, model.AvailableSeatsCount));
+
+        //    if (summary.IsCreated)
+        //    {
+        //        return Ok();
+        //    }
+        //    else
+        //    {
+        //        return BadRequest(summary.Message);
+        //    }
+        //}
+    }
+}
